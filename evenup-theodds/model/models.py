043d42053b4deffad558ca49059c764916b9cd84
eryktr <eryk.trzeciakiewicz@gmail.com>
2020-02-25 21:49:15 +0100

    added model

---
+++ evenup-theodds/model/models.py
@@ -0,0 +1,56 @@
+import enum
+from typing import List
+from dataclasses import dataclass
+
+
+class Division(enum.Enum):
+    I4 = "Iron IV"
+    I3 = "Iron III"
+    I2 = "Iron II"
+    I1 = "Iron I"
+    B4 = "Bronze IV"
+    B3 = "Bronze III"
+    B2 = "Bronze II"
+    B1 = "Bronze I"
+    S4 = "Silver IV"
+    S3 = "Silver III"
+    S2 = "Silver II"
+    S1 = "Silver I"
+    G4 = "Gold IV"
+    G3 = "Gold III"
+    G2 = "Gold II"
+    G1 = "Gold I"
+    P4 = "Plat IV"
+    P3 = "Plat III"
+    P2 = "Plat II"
+    P1 = "Plat I"
+    D4 = "Diamond IV"
+    D3 = "Diamond III"
+    D2 = "Diamond II"
+    D1 = "Diamond I"
+    M = "Master"
+    MS1 = "Master Split 1"
+    MS2 = "Master Split 2"
+    MS3 = "Master Split 3"
+    G = "Grandmaster"
+    GS1 = "Grandmaster Split 1"
+    GS2 = "Grandmaster Split 2"
+    GS3 = "Grandmaster Split 3"
+    C = "Challenger"
+    CS1 = "Challenger Split 1"
+    CS2 = "Challenger Split 2"
+    CS3 = "Challenger Split 3"
+
+    @classmethod
+    def fromstr(cls, string: str) -> "Division":
+        return cls(str)
+
+    @classmethod
+    def allowed_division_names(cls) -> List[str]:
+        return [div.value for div in cls]
+
+
+@dataclass(fronzen=True)
+class Player:
+    nick: str
+    division: Division
